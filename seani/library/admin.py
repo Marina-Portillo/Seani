c22aec40540946a86c789c580584d1e95970ecf6
Marina <maryypucca@gmail.com>
2024-02-22 13:16:43 -0600

    04 implements Cloudinary customize Models

--- seani/library/admin.py
+++ seani/library/admin.py
@@ -2,7 +2,19 @@ from django.contrib import admin
 
 from .models import Module, Question
 
+@admin.register(Question)
+class QuesgtionAdmin(admin.ModelAdmin):
+    list_display = ['pk','question_text', 'module', 'correct']
+    list_filter = ['module']
+
+
+class QuesgtionInline(admin.StackedInline):
+    model = Question
 
 # Register your models here.
-admin.site.register(Module)
-admin.site.register(Question)
+@admin.register(Module)
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'num_questions']
+    inlines = [QuesgtionInline]
+
+
--- seani/library/models.py
+++ seani/library/models.py
@@ -1,19 +1,26 @@
 from django.db import models
 
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
 class Module(models.Model):
     name = models.CharField(
         verbose_name = "Nombre",
-        max_length=100)
+        max_length=100,)
     description = models.CharField(
-        verbose_name = "Descripción",
-        max_length=200)
+        verbose_name = 'Description',
+        max_length = 200)
+    
+    
+    def num_questions(self):
+        return self.question_set.count()
+        num_questions.short_description = 'Número de Preguntas'
     
     def __str__(self):
         return self.name
     
-    class Meta:
+    class Meta: 
         verbose_name = 'módulo'
         verbose_name_plural = 'módulos'
 
@@ -21,32 +28,45 @@ class Question(models.Model):
     module = models.ForeignKey(
         Module, 
         on_delete = models.CASCADE,
-        verbose_name = 'Módulo')
+        verbose_name = 'Modulo')
     question_text = models.CharField(
-        verbose_name = 'Texto de la Pregunta',
-        max_length=200, null = True)
-    question_image = models.ImageField(
-        verbose_name= 'Imagen de la Pregunta',
-        upload_to='questions', null = True, blank=True)
+        verbose_name = 'Texto de la pregunta',
+        max_length=200, null = True, blank = True)
+# question_image = models.ImageField(
+#    verbose_name = "Imagen de la pregunta",
+#   upload_to='questions', null = True, blank = True)
+    question_image = CloudinaryField(
+        verbose_name = 'Imagen de la Pregunta',
+        folder = 'questions',
+        resource_type = 'image',
+        null = True, blank = True
+    )
+
     answer1 = models.CharField(
         verbose_name = 'Respuesta A',
-        max_length=200)
+        max_length=200)  
+    
+    
     answer2 = models.CharField(
         verbose_name = 'Respuesta B',
         max_length=200)
+    
     answer3 = models.CharField(
         verbose_name = 'Respuesta C',
-        max_length=200, null = True, blank=True)
+        max_length=200, null = True, blank = True)
+    
     answer4 = models.CharField(
         verbose_name = 'Respuesta D',
-        max_length=200, null = True, blank=True)
+        max_length=200, null = True, blank = True)
+    
     correct = models.CharField(
-        verbose_name = 'Respuesta correcta',
+        verbose_name = 'Respuesta Correcta',
         max_length=5)
     
-    def __str__(self):
-        return f"{ self.module} - { self.id}"
     
-    class Meta:
-        verbose_name = 'pregunta'
-        verbose_name_plural = 'preguntas'
+    def __str__(self):
+        return f"{ self.module } - { self.id }"
+
+    class Meta: 
+        verbose_name = 'respuesta'
+        verbose_name_plural = 'respuestas'
