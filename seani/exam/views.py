b5b9978a3b97496c9220cfdcd2d64b26e473582e
Marina <maryypucca@gmail.com>
2024-03-21 12:44:14 -0600

    08 Implements copute of score

--- seani/exam/views.py
+++ seani/exam/views.py
@@ -1,20 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 
-from.models import Exam
+from .models import Exam
 from .forms import CandidateForm
+from django.contrib.auth.decorators import login_required
 
+
+
+@login_required
 def home(request):
-    User = request.user
-    return render(request, 'exam/home.html', {'user: user'})
+        user = request.user
+        if user.is_superuser:
+            return redirect('admin:index')
+        return render(request, 'exam/home.html', {'user':user})
 
-# Create your views here.
+@login_required
+def question(request, m_id, q_id = 1 ):
+    exam = request.user.exam
+    if request.method == 'POST':
+        answer = request.POST['answer']
+        questions = exam.breakdown_set.filter(question__module_id=m_id)
+        question = questions[q_id - 1]
+        question.answer = answer
+        question.save()
+        return redirect('exam:question', m_id, q_id + 1)
+
+    
+    try:
+        questions = exam.breakdown_set.filter(question__module_id=m_id)
+        question = questions[q_id - 1].question
+        answer = questions[q_id -1].answer
+        return render(request, 'exam/question.html', {
+            'question': question,
+            'correct': answer,
+            'm_id': m_id,
+            'q_id': q_id,
+            })
+    except IndexError:
+        return redirect('exam:home')
+
+
+@login_required
 def add_candidate(request):
     if request.method == 'POST':
         form = CandidateForm(request.POST)
         if form.is_valid():
-            #recibir datos
+            
+            #Recibir datos
             first_name = form.cleaned_data['first_name']
             last_name = form.cleaned_data['last_name']
             username = form.cleaned_data['username']
@@ -22,24 +55,26 @@ def add_candidate(request):
             password = form.cleaned_data['password']
             career = form.cleaned_data['career']
             stage = form.cleaned_data['stage']
-            #crear usuario
-            user = User.objects.create_user(username,email,password)
+            
+            #Crear usuario
+            user = User.objects.create_user(username, email, password)
             user.first_name = first_name
             user.last_name = last_name
             user.save()
 
-            #crear examen
+            #Crear examen
             exam = Exam.objects.create(
                 user=user,
                 career=career,
-                stage=stage)
-
-            #llenar examen
+                stage=stage
+            )
+            
+            #Llenar examen
             exam.set_modules()
             exam.set_questions()
             return HttpResponse("Usuario y examen creado!")
-
+    
     form = CandidateForm()
-    return render(request,'exam/add_candidate.html',
+    return render(request, 
+                    'exam/add_candidate.html',
                     {'form': form})
-
